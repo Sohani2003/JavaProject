38fd71489afc276446c316a4228906d8740ae3a8
Sohani Chakraborty <sohani.chakraborty@gmail.com>
2025-02-25 11:03:10 +0530

    addition done

--- gitdemonew/src/gitdemonew/Testnew.java
+++ gitdemonew/src/gitdemonew/Testnew.java
@@ -4,7 +4,10 @@ public class Testnew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a=9;
+		int b = 6;
+		int c =a/b;
+		System.out.println(c);
 	}
 
 }
